a31b72e7b66cd35d4116fb3cef165a6724bc34de
AnisShrestha11 <Anis.shrestha11@gmail.com>
2018-11-02 17:43:58 +0545

    Addition of multiple pages

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('landing-page', { title: 'landing' });
+  res.render('landing-page', { title: 'Landing-page' });
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'login' });
+  res.render('login', { title: 'Login' });
 });
 
 router.get('/page-forgot', function(req, res, next) {
-  res.render('page-forgot', { title: 'forgot' });
+  res.render('page-forgot', { title: 'Forgot Password' });
+});
+
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Events' });
+});
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard', { title: 'Dashboard' });
 });
 
 router.get('/page-signup', function(req, res, next) {
-  res.render('page-signup', { title: 'page-signup' });
+  res.render('page-signup', { title: 'Page-signup' });
+});
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { title: 'Profile' });
+});
+
+router.get('/add-donation', function(req, res, next) {
+  res.render('add-donation', { title: 'Add Donation' });
 });
 
 module.exports = router;
